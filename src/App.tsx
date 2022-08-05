7f02b54f2ad5365676f471330b2776d2618c8151
Giovani Silva <giovani.silva@live.com>
2022-08-05 20:19:15 -0300

    theme global e default

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { ThemeProvider } from 'styled-components';
+
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
+
 export function App() {
-  return <h1>POMODORO</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>POMODORO</h1>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
